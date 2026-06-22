d335523d426e90b0cb3f2be3eff833ec3304aa53
ethanzhou <ethanzhou@futurefab.ai>
2026-06-22 15:46:31 +0800

    feat(1.0.5): sqltoy navigator
    1. 添加 INSERT 语句的语法高亮
    2. 添加常量定义 SQL 跳转标识
    3. 变更图标

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.ax.sqltoy"
-version = "1.0.4"
+version = "1.0.5"
 
 dependencies {
     intellijPlatform {
@@ -20,8 +20,14 @@ intellijPlatform {
         }
 
         changeNotes = """
-            Initial version: navigate Java SqlToy sqlId string literals to XML &lt;sql id="..."&gt; definitions
-            and inject IDEA SQL support inside SqlToy XML SQL tags when available.
+            Change version :
+                1. Add syntax highlighting for INSERT statements.
+                2. Add constant definition for SQL jump identifier.
+                3. Change icon.
+                
+                1. 添加 INSERT 语句的语法高亮
+                2. 添加常量定义 SQL 跳转标识
+                3. 变更图标
         """.trimIndent()
     }
 }
